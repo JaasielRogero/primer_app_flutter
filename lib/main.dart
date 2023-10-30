8a5856d1f10aeaadc262e8fc50cae8520c21ec42
Jaasiel Rogero <jaasiel.rogero@gmail.com>
2023-10-30 12:58:25 -0700

    Se convirtio myHomePage a stateful

--- lib/main.dart
+++ lib/main.dart
@@ -43,9 +43,15 @@ class MyAppState extends ChangeNotifier{
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex=0;
   @override
     Widget build(BuildContext context){
 
@@ -63,9 +69,11 @@ class MyHomePage extends StatelessWidget {
                   icon: Icon(Icons.favorite), 
                   label: Text("Favoritos"))
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value){
-                print("Seleccion: $value");
+                setState(() {
+                  selectedIndex=value;
+                }); 
               },
             )
           ),
