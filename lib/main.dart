e7a0fff7b95aac6fa2fb067dfe5990c23401a381
Jaasiel Rogero <jaasiel.rogero@gmail.com>
2023-11-01 11:29:43 -0700

    Se agrego placeHolder

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,13 @@ class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex=0;
   @override
     Widget build(BuildContext context){
-
+      Widget page;
+      switch(selectedIndex){
+        case 0: page=GeneratorPage(); break;
+        case 1: page=Placeholder(); break;
+        default:
+          throw UnimplementedError("No hay un widget para: $selectedIndex ");
+      }
     return Scaffold(
       body: Row(
         children: [
@@ -80,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),)),
+              child: page,)),
         ],)
     );
   }
